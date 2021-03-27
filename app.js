de67231615e0743d49f3e48ea388f6b4cd8aa8dd
Yuchi Su <astraiostor@gmail.com>
2021-03-27 15:21:27 +0800

    feat: add login and register route and page

--- app.js
+++ app.js
@@ -12,8 +12,36 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+// 設定路由
+// 首頁路由
 app.get('/', (req, res) => {
   res.send('hello world')
+})
+
+// 認證系統的路由
+// 登入畫面路由
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+// 登入 submit
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+// 註冊畫面路由
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+// 註冊 submit
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+
+// 登出路由
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
 })
 
 app.listen(PORT, () => {
