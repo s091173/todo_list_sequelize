3bdc741ab1260e455049f0963b4b4b9c1b7d95e1
Yuchi Su <astraiostor@gmail.com>
2021-03-28 18:35:47 +0800

    feat: add facebook login & dotenv

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
 const bcrypt = require('bcryptjs')
 
 // 載入 User Model
@@ -10,6 +11,7 @@ module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
 
+  // Local Strategy
   passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
     User.findOne({ where: { email } }) // Sequelize 查詢特定 User 的 email 
       .then(user => {
@@ -25,6 +27,34 @@ module.exports = app => {
       })
       .catch(err => done(err, false))
   }))
+
+  // Facebook Strategy
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  },
+    (accessToken, refreshToken, profile, done) => {
+      const { name, email } = profile._json
+      User.findOne({ where: { email } })
+        .then(user => {
+          if (user) return done(null, user)
+
+          const randomPassword = Math.random().toString(36).slice(-8)
+          bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(randomPassword, salt))
+            .then(hash => User.create({
+              name,
+              email,
+              password: hash
+            }))
+            .then(user => done(null, user))
+            .catch(err => done(err, false))
+        })
+    }
+  ))
 
   passport.serializeUser((user, done) => {
     done(null, user.id)
--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,11 @@ const { authenticator } = require('../middleware/auth')
 const home = require('./modules/home')
 const users = require('./modules/users')
 const todos = require('./modules/todos')
+const auth = require('./modules/auth')
 
 router.use('/todos', authenticator, todos)
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
